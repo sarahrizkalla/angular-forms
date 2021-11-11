ca291ffbb015134ac509061ae1a66a3aa7d30da1
Sarah Rizkalla <sarah.rizkalla16@gmail.com>
2021-11-11 14:29:26 -0500

    Add two-way data binding to our angular forms.

--- ps-demo/src/app/user-settings-form/user-settings-form.component.ts
+++ ps-demo/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -6,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-settings-form.component.css']
 })
 export class UserSettingsFormComponent implements OnInit {
+  originalUserSettings: UserSettings = {
+    name: 'Milton',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: "Here are some notes..."
+  };
 
+  userSettings: UserSettings = {...this.originalUserSettings}; //use spread operator to make a copy of our original user settings
   constructor() { }
 
   ngOnInit(): void {
